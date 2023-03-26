b91339ad0748ced8df33c1c1ff1805a8e4313fed
junkinside <erkinkurmanbekov3@gmail.com>
2023-03-26 23:10:24 +0600

    added CORS

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,19 @@ const app = express();
 const path = require('path');
 const { logger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
