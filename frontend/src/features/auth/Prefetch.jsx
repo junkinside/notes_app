d1eb9a283788438ac6614c635345f0abed702101
junkinside <erkinkurmanbekov3@gmail.com>
2023-04-05 21:55:53 +0600

    added form for notes CRUD

--- frontend/src/features/auth/Prefetch.jsx
+++ frontend/src/features/auth/Prefetch.jsx
@@ -6,12 +6,10 @@ import { Outlet } from 'react-router-dom';
 
 const Prefetch = () => {
   useEffect(() => {
-    console.log('Subscribing');
     const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate());
     const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
 
     return () => {
-      console.log('Unsubscribing');
       notes.unsubscribe();
       users.unsubscribe();
     };
